9b2726bf4c1e8629fa8f724b9ae13c1e67888cc8
Mikhaela <mikhaelatapia@gmail.com>
2025-11-03 11:32:04 +0800

    remove backend from main repo

--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import hello, chat, end
-
-urlpatterns = [
-    path('hello/', hello),
-    # path('start/', start),
-    path('chat/', chat),
-    path('end/', end)
-]
